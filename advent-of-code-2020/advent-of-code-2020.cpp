350b685a7432b71477a970557eba496b858938ab
Francesco Stiffoni <fstiffo@sgajo.com>
2020-11-29 12:07:27 +0100

    First commit

---
+++ advent-of-code-2020/advent-of-code-2020.cpp
@@ -0,0 +1,12 @@
+﻿// advent-of-code-2020.cpp : Defines the entry point for the application.
+//
+
+#include "advent-of-code-2020.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

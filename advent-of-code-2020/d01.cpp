8e63b95d41376e53341d74467622d4901b2eacfb
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-01 10:39:26 +0100

    Untabify

--- advent-of-code-2020/d01.cpp
+++ advent-of-code-2020/d01.cpp
@@ -5,37 +5,37 @@
 
 int d01_first_half()
 {
-	std::vector<std::string> vecOfStr;
+    std::vector<std::string> vecOfStr;
 
-	auto result = getFileContent("d01input.txt", vecOfStr);
+    auto result = getFileContent("d01input.txt", vecOfStr);
 
-	if (result) {
-		std::vector<int> input;
-		std::transform(vecOfStr.begin(), vecOfStr.end(), std::back_inserter(input),
-			[](const std::string& str) -> int { return std::stoi(str); });
-		const auto size = input.size();
-		for (auto first = 0; first < size - 1; first++)
-			for (auto second = first + 1; second < size; second++)
-				if (input[first] + input[second] == 2020) return input[first] * input[second];
-	}
-	return -1;
+    if (result) {
+        std::vector<int> input;
+        std::transform(vecOfStr.begin(), vecOfStr.end(), std::back_inserter(input),
+            [](const std::string& str) -> int { return std::stoi(str); });
+        const auto size = input.size();
+        for (auto first = 0; first < size - 1; first++)
+            for (auto second = first + 1; second < size; second++)
+                if (input[first] + input[second] == 2020) return input[first] * input[second];
+    }
+    return -1;
 }
 
 int d01_second_half()
 {
-	std::vector<std::string> vecOfStr;
+    std::vector<std::string> vecOfStr;
 
-	auto result = getFileContent("d01input.txt", vecOfStr);
+    auto result = getFileContent("d01input.txt", vecOfStr);
 
-	if (result) {
-		std::vector<int> input;
-		std::transform(vecOfStr.begin(), vecOfStr.end(), std::back_inserter(input),
-			[](const std::string& str) -> int { return std::stoi(str); });
-		const auto size = input.size();
-		for (auto first = 0; first < size - 2; first++)
-			for (auto second = first + 1; second < size - 1; second++)
-				for (auto third = second + 1; third < size; third++)
-					if (input[first] + input[second] + input[third] == 2020) return input[first] * input[second] * input[third];
-	}
-	return -1;
+    if (result) {
+        std::vector<int> input;
+        std::transform(vecOfStr.begin(), vecOfStr.end(), std::back_inserter(input),
+            [](const std::string& str) -> int { return std::stoi(str); });
+        const auto size = input.size();
+        for (auto first = 0; first < size - 2; first++)
+            for (auto second = first + 1; second < size - 1; second++)
+                for (auto third = second + 1; third < size; third++)
+                    if (input[first] + input[second] + input[third] == 2020) return input[first] * input[second] * input[third];
+    }
+    return -1;
 }
--- advent-of-code-2020/utils.cpp
+++ advent-of-code-2020/utils.cpp
@@ -9,23 +9,23 @@
  */
 bool getFileContent(std::string fileName, std::vector<std::string>& vecOfStrs)
 {
-	// Open the File
-	std::ifstream in(fileName.c_str());
-	// Check if object is valid
-	if (!in)
-	{
-		std::cerr << "Cannot open the File : " << fileName << std::endl;
-		return false;
-	}
-	std::string str;
-	// Read the next line from File untill it reaches the end.
-	while (std::getline(in, str))
-	{
-		// Line contains string of length > 0 then save it in vector
-		if (str.size() > 0)
-			vecOfStrs.push_back(str);
-	}
-	//Close The File
-	in.close();
-	return true;
+    // Open the File
+    std::ifstream in(fileName.c_str());
+    // Check if object is valid
+    if (!in)
+    {
+        std::cerr << "Cannot open the File : " << fileName << std::endl;
+        return false;
+    }
+    std::string str;
+    // Read the next line from File untill it reaches the end.
+    while (std::getline(in, str))
+    {
+        // Line contains string of length > 0 then save it in vector
+        if (str.size() > 0)
+            vecOfStrs.push_back(str);
+    }
+    //Close The File
+    in.close();
+    return true;
 }
